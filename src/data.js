eb3fcb59f143e228b401ee4c9aa0a2fb7ed09117
[yair-r] <yair7705778@gmail.com>
2022-02-13 21:31:54 +0200

    123

--- src/data.js
+++ src/data.js
@@ -4,54 +4,58 @@ export const items = [
     capacity: '1',
     category: 'Table for 1 person  ',
     title: 'Time 00-00-00',
+    Concat: [102, 103],
   },
   {
     id: '102',
     capacity: '1',
     category: 'Table for 1 person  ',
     title: 'Time 00-00-00',
+    Concat: [101, 103],
   },
   {
     id: '103',
     capacity: '1',
     category: 'Table for 1 person  ',
     title: 'Time 00-00-00',
+    Concat: [102, 102],
   },
-
-
-
 
   {
     id: '201',
     capacity: '2',
     category: 'Table for 2 person ',
     title: 'Time 00-00-00',
+    Concat: [202],
   },
   {
     id: '202',
     capacity: '2',
     category: 'Table for 2 person ',
     title: 'Time 00-00-00',
+    Concat: [201],
   },
   {
     id: '203',
     capacity: '2',
     category: 'Table for 2 person ',
     title: 'Time 00-00-00',
+    Concat: [601],
   },
-
 
   {
     id: '301',
     capacity: '3',
     category: 'Table for 3 person  ',
     title: 'Time 00-00-00',
+    Concat: [],
   },
   {
     id: '302',
     capacity: '3',
     category: 'Table for 3 person  ',
     title: 'Time 00-00-00',
+    Concat: [501],
   },
 
   {
@@ -59,23 +63,27 @@ export const items = [
     capacity: '4',
     category: 'Table for 4 person  ',
     title: 'Time 00-00-00',
+    Concat: [402],
   },
   {
     id: '402',
     capacity: '4',
     category: 'Table for 4 person  ',
     title: 'Time 00-00-00',
+    Concat: [401],
   },
   {
     id: '501',
     capacity: '5',
     category: 'Table for 5 person',
     title: 'Time 00-00-00',
+    Concat: [302],
   },
   {
     id: '601',
     capacity: '6',
     category: 'Table for 6 person',
     title: 'Time 00-00-00',
+    Concat: [203],
   },
 ];
