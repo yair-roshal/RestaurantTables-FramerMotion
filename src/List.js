e44280bab3792ff8815178d0cae66889b1fadc7a
[yair-r] <yair7705778@gmail.com>
2022-02-13 23:27:16 +0200

    123

--- src/List.js
+++ src/List.js
@@ -1,41 +1,54 @@
-import React from "react";
-import { items } from "./data";
-import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
-
-
+import React from 'react';
+import { items } from './data';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 export function List({ selectedId }) {
-  return (
-    <ul className="card-list">
-      {items.map(card => (
-        <Card key={card.id} {...card} isSelected={card.id === selectedId} />
-      ))}
-    </ul>
-  );
+    return (
+        <ul className="card-list">
+            {items.map((card) => (
+                <Card
+                    key={card.id}
+                    {...card}
+                    isSelected={card.id === selectedId}
+                />
+            ))}
+        </ul>
+    );
 }
 
 function Card({ id, capacity, title, category, theme }) {
-  return (
-    <li className={`card ${theme}`}>
-      <div className="card-content-container">
-        <motion.div className="card-content" layoutId={`card-container-${id}`}>
-          <motion.div
-            className="card-image-container"
-            layoutId={`card-image-container-${id}`}
-          >
-            <img className="card-image" src={`images/${capacity}.jpg`} alt="" />
-          </motion.div>
-          <motion.div
-            className="title-container"
-            layoutId={`title-container-${id}`}
-          >
-            <span className="category">{category}</span>
-            <h2>{title}</h2>
-          </motion.div>
-        </motion.div>
-      </div>
-      <Link to={id} className={`card-open-link`} />
-    </li>
-  );
+    return (
+        <li className={`card ${theme}`}>
+            <div className="card-content-container">
+                <motion.div
+                    className="card-content"
+                    layoutId={`card-container-${id}`}
+                >
+                    <motion.div
+                        className="card-image-container"
+                        layoutId={`card-image-container-${id}`}
+                    >
+                        <img
+                            className="card-image"
+                            src={`images/${capacity}.jpg`}
+                            alt=""
+                        />
+                    </motion.div>
+
+                    <motion.div
+                        className="title-container"
+                        layoutId={`title-container-${id}`}
+                    >
+                        <span className="category">{category}</span>
+                        <h2>{title}</h2>
+                        <h2>{id}</h2>
+
+                    </motion.div>
+
+                </motion.div>
+            </div>
+            <Link to={id} className={`card-open-link`} />
+        </li>
+    );
 }
