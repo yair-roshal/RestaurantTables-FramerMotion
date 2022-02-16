4c8f2ab93daa2408a6f701a83604e66c9e544d24
yair <yair7705778@gmail.com>
2022-02-16 17:04:00 +0200

    mobx

--- src/List.jsx
+++ src/List.jsx
@@ -1,53 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { items } from './data';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+import { tablesStore } from './Logic';
+
+// useEffect(() => {
+//   console.log('TablesStore.tables  :>> ', tablesStore.tables )
+// }, []);
+
+let tables =  tablesStore.tables 
+
 export function List({ selectedId }) {
-    return (
-        <div className="card-list">
-            {items.map((card) => (
-                <Card
-                    key={card.id}
-                    {...card}
-                    isSelected={card.id === selectedId}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div className="card-list">
+  {console.log('...tables :>> ', {...tables} )} 
+ 
+      {items.map((card) => (
+        <Card key={card.id} {...card} isSelected={card.id === selectedId} />
+      ))}
+    </div>
+  );
 }
 
-function Card({ id, capacity, title, category   }) {
-    return (
-        <div className={`card  `}>
+function Card({ id, capacity, title, category }) {
+  return (
+    <div className={`card  `}>
+      <div className="card-content-container">
+        <motion.div className="card-content" layoutId={`card-container-${id}`}>
+          <motion.div
+            className="card-image-container"
+            layoutId={`card-image-container-${id}`}
+          >
+            <img className="card-image" src={`images/${capacity}.jpg`} alt="" />
+          </motion.div>
 
-            <div className="card-content-container">
-                <motion.div
-                    className="card-content"
-                    layoutId={`card-container-${id}`}
-                >
-                    <motion.div
-                        className="card-image-container"
-                        layoutId={`card-image-container-${id}`}
-                    >
-                        <img
-                            className="card-image"
-                            src={`images/${capacity}.jpg`}
-                            alt=""
-                        />
-                    </motion.div>
+          <motion.div
+            className="title-container"
+            layoutId={`title-container-${id}`}
+          >
+            <span className="category">{category}</span>
+            <h5>{title}</h5>
+          </motion.div>
+          <motion.div className="round">
+            <h5>{id}</h5>
+          </motion.div>
+        </motion.div>
+      </div>
 
-                    <motion.div
-                        className="title-container"
-                        layoutId={`title-container-${id}`}
-                    >
-                        <span className="category">{category}</span>
-                        <h5>{title}</h5>
-                        <h5>{id}</h5>
-                    </motion.div>
-                </motion.div>
-            </div>
-            <Link to={id} className={`card-open-link`} />
-        </div>
-    );
+      <Link to={id} className={`card-open-link`} />
+    </div>
+  );
 }
--- src/Logic.jsx
+++ src/Logic.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import { items } from './data';
+import { makeAutoObservable } from 'mobx';
+import { observer } from 'mobx-react';
+
+
+class TablesStore {
+  _tables = items ;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  // setTablesClass() {
+  //   this._tables = setTables(
+  //     tables.concat({
+  //       id: '101',
+  //       capacity: '1',
+  //       category: 'Table for 1 person  ',
+  //       title: 'Time 00-00-00',
+  //       Concat: [102, 103],
+  //     }),
+  //   );
+  // }
+
+  get tables() {
+    return this._tables;
+  }
+}
+
+export const tablesStore = new TablesStore();
+
+
+{  console.log(' tablesStore :>> ', tablesStore);
+}
+
+
 
 export function Logic() {
   const [seconds, setSeconds] = React.useState(20);
   const [timerActive, setTimerActive] = React.useState(false);
+  const [tables, setTables] = React.useState(items);
 
   React.useEffect(() => {
     if (seconds > 0 && timerActive) {
@@ -18,9 +55,20 @@ export function Logic() {
     console.log('SearchFreeTable :>> ');
     console.log(seconds);
 
+    setTables(
+      tables.concat({
+        id: '101',
+        capacity: '1',
+        category: 'Table for 1 person  ',
+        title: 'Time 00-00-00',
+        Concat: [102, 103],
+      }),
+    );
 
-	
+    console.log('tables ', tables);
   }
+
+
 
   return (
     <div className="buttonStart">
